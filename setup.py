43f54c53da2bfc003811b2637a309a2641661e9b
zhile-io <admin@zhile.io>
2023-10-13 19:12:40 +0800

    upgrade pandora-cloud
    Signed-off-by: zhile-io <admin@zhile.io>

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     install_requires=requirements,
     extras_require={
         'api': requirements_api,
-        'cloud': ['pandora-cloud~=0.6.1'],
+        'cloud': ['pandora-cloud~=0.7.1'],
     },
     entry_points={
         'console_scripts': [
